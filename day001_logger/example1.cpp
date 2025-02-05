3c47d69098caa0b56000eaaed4a11b43a7a81b29
willydlw <willydlw@comcast.net>
2025-02-05 10:33:55 -0700

    added assert to example1.cpp

--- day001_logger/example1.cpp
+++ day001_logger/example1.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
+#include <cassert>
 
 int average(int* arr, int n)
 {
-    int sum = 0;
     int avg = 0;
-
-    for(int i = 0; i < n; i++){
-        sum += arr[i];
-    }
 
     if(n == 0){
         std::cerr   << "File: " << __FILE__ 
-                    << ", function: " << __func__ 
+                    << ", function: " << __FUNCTION__
                     << ", line: " << __LINE__ 
                     << ", Division by Zero Error, n = " << n 
                     << ", returning 0\n";
     }
     else if(n < 0){
         std::cerr   << "File: " << __FILE__ 
-                    << ", function: " << __func__ 
+                    << ", function: " << __FUNCTION__ 
                     << ", line: " << __LINE__ 
                     << ", Error, n = " << n 
                     << ", returning 0\n";
     }
     else{
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
         avg = sum / n;
     }
 
@@ -37,16 +37,20 @@ int main()
     int avg;
 
     // generate divide by zero error
+    std::cout << "\n*** Testing Divide By Zero ***\n";
     avg = average(arr, 0);
-    std::cout << "average function returned " << avg << "\n\n";
+    assert(avg == 0);
+
 
     // negative n error 
+    std::cout << "\n*** Testing n < 0 ***\n";
     avg = average(arr, -1);
-    std::cout << "average function returned " << avg << "\n\n";
+    assert(avg == 0);
 
     // no errors
+    std::cout << "\n*** Testing Good Case ***\n";
     avg = average(arr, 3);
-    std::cout << "average function returned " << avg << "\n\n";
+    assert(avg == 2);
 
     return 0;
 }
